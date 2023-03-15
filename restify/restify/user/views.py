f7c8798b901be1036264c25badee395afb1cdf8c
TEJMaster <69652431+TEJMaster@users.noreply.github.com>
2023-03-15 19:21:33 -0400

    push

--- restify/restify/user/views.py
+++ restify/restify/user/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, login, logout
 from .serializers import UserSerializer, LogInSerializer
 from rest_framework import generics
 from rest_framework.response import Response
@@ -8,7 +8,7 @@ from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework_simplejwt.authentication import JWTAuthentication
-
+from rest_framework.permissions import IsAuthenticated
 
 
 # Create your views here.
@@ -33,12 +33,17 @@ class UserLoginAPIView(APIView):
         user = authenticate(username=username, password=password)
 
         if user is not None:
+            login(request, user)
             refresh = RefreshToken.for_user(user)
-            response = Response({'message': 'Login successful!'})
+            response = Response({'message': 'Login successful! Welcome ' + username + '!'})
 
             # Set the access and refresh token cookies
             response.set_cookie('refresh_token', str(refresh), httponly=True)
             response.set_cookie('access_token', str(refresh.access_token), httponly=True)
+
+            # Set the JWT authentication header
+            jwt_token = str(refresh.access_token)
+            response['Authorization'] = 'Bearer ' + jwt_token
 
             return response
         else:
@@ -46,7 +51,9 @@ class UserLoginAPIView(APIView):
     
 
 class UserLogoutAPIView(APIView):
-    authentication_classes = [JWTAuthentication]
+    serializer_class = LogInSerializer
+    authentication_classes = []
+    permission_classes = []
 
     def get(self, request, *args, **kwargs):
         try:
@@ -55,16 +62,43 @@ class UserLogoutAPIView(APIView):
             if not refresh_token:
                 return Response({'message': 'User is already logged out.'})
 
+            user = request.user
+            username = user.username
+            
             # Blacklist the refresh token
+
             token = RefreshToken(refresh_token)
             token.blacklist()
 
             # Clear the access and refresh token cookies
-            response = Response({'message': 'User successfully logged out.'})
+            response = Response({'message': 'User ' + username + 'is successfully logged out.'})
             response.delete_cookie('refresh_token')
             response.delete_cookie('access_token')
-
             return response
 
         except Exception as e:
             return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserProfileAPIView(APIView):
+    
+    permission_classes = [IsAuthenticated]
+    
+    def get(self, request):
+        # Get the user object
+        user = request.user
+        
+        # Get the user's information
+        user_info = {
+            'id': user.id,
+            'username': user.username,
+        }
+        
+        # Serialize the user data and return it in the response
+        serializer = UserSerializer(user)
+        data = {
+            'user': user_info,
+            'profile': serializer.data
+        }
+        return Response(data)
+
