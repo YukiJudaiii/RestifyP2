15b660d986762a4117a8f2bf15e7ff1cd3638f03
Houyi Chen <houyi.chen@mail.utoronto.ca>
2023-03-16 13:46:17 -0400

    pagination

--- restify/restify/property/views.py
+++ restify/restify/property/views.py
@@ -55,9 +55,18 @@ class PropertyDeleteAPIView(DestroyAPIView):
     serializer_class = PropertySerializer
 
 class PropertyPagination(pagination.PageNumberPagination):
-    page_size = 10
+    page_size = 2
     page_size_query_param = 'page_size'
     max_page_size = 100
+    def get_paginated_response(self, data):
+        return Response({
+            'links': {
+                'next': self.get_next_link(),
+                'previous': self.get_previous_link()
+            },
+            'count': self.page.paginator.count,
+            'results': data
+        })
     
     
 class PropertySearchView(ListAPIView):
