e656a958404634d20c7083c566204dab09417715
Houyi Chen <houyi.chen@mail.utoronto.ca>
2023-03-16 22:31:57 -0400

    fixing notification

--- restify/restify/notification/views.py
+++ restify/restify/notification/views.py
@@ -4,10 +4,17 @@ from .serializers import NotificationSerializer
 from accounts.models import CustomUser
 from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
+from rest_framework.pagination import PageNumberPagination
 
+class NotificationPagination(PageNumberPagination):
+    page_size = 2
+    page_size_query_param = 'page_size'
+    max_page_size = 1000
+    
 class NotificationListView(generics.ListAPIView):
     serializer_class = NotificationSerializer
     permission_classes = [permissions.IsAuthenticated]
+    pagination_class = NotificationPagination
 
     def get_queryset(self):
         u = get_object_or_404(CustomUser, username=self.request.user.username)
@@ -23,11 +30,13 @@ class NotificationReadView(generics.UpdateAPIView):
     
     def perform_update(self, serializer):
         instance = self.get_object()
+        # u = get_object_or_404(CustomUser, username=self.request.user.username)
+
         instance.is_read = True
         instance.save()
         instance.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
-    
+
 
 class NotificationClearView(generics.DestroyAPIView):
     serializer_class = NotificationSerializer
